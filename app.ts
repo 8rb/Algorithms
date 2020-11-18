a312c1eb451c1aa33f70f508cb6b957c08993c24
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-11-18 14:24:48 -0500

    Inital Commit

---
+++ app.ts
@@ -0,0 +1,20 @@
+export {};
+
+console.log("Hello Typescript");
+
+function welcomePerson(person: Person):string {
+    console.log(`Hey ${person.firstname} ${person.lastname}`);
+    return "GAAAAA";
+}
+
+const roro = {
+    firstname: "Rodrigo",
+    lastname: "Ramirez"
+}
+
+welcomePerson(roro);
+
+interface Person {
+    firstname: string,
+    lastname: string
+}
