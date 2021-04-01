32abb61a95bc74557a184989869ec270a0d17d7e
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-04-01 12:32:24 -0500

    ⚡️ Update 1.1
    Update the method to check if a key exists to be more efficient; add a brief explanation of the algorithm.

--- 1 Arrays and Strings/1_1.py
+++ 1 Arrays and Strings/1_1.py
@@ -6,12 +6,17 @@
 def is_unique(string):
     chars = {}
     for c in string:
-        if(chars.get(c)):
+        if(c in chars):
             return False
         else:
             chars[c] = 1
     return True
 
-
-ans = is_unique("HELLO")
+ans = is_unique("HELO")
 print(ans)
+
+# Time complexity: Amortized O(n) where n is the length of the string
+# Amortized since adding a value to a dictionary is amortized O(1)
+# And checking if a key exists is also amortized O(1)
+# Pros: Algorithm will stop when it finds the first repeated character inside
+# the string, so most of the times, runtime would be less than O(n)
