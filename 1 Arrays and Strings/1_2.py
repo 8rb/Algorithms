e268f3e211b9c99bd847f6e66c6a949442be3794
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-04-02 15:07:16 -0500

    ⚡️ New Solution to 1.2
    Add new solution to 1.2 problem "Check Permutation". This new solution has a runtime of O(N).

--- 1 Arrays and Strings/1_2.py
+++ 1 Arrays and Strings/1_2.py
@@ -1,9 +1,9 @@
-# Check-Permutation
+# 1.2 Check-Permutation
 
 # Given two strings, write a method to decide if one is 
 # a permutation of the other
 
-# First Answer is O(NlogN) since the sorting operation
+# First Solution is O(NlogN) since the sorting operation
 # is O(NlogN) and string comparison is at most O(n)
 
 def check_permutation(s1, s2):
@@ -16,3 +16,28 @@ def check_permutation(s1, s2):
 
 print(check_permutation("ABBBBBB", "BBBABBB"))
 
+# Second Solution is O(N) since we iterate over each string
+# and adding/accesing/modifying a value in a dictionary has
+# an amortized complexity of O(1)
+
+def check_permutation2(s1, s2):
+    dic1 = {}
+    dic2 = {}
+    for c in s1:
+        if(c in dic1):
+            dic1[c]+=1
+        else:
+            dic1[c]=1
+
+    for c in s2:
+        if(c in dic2):
+            dic2[c]+=1
+        else:
+            dic2[c]=1
+    
+    if(dic1 == dic2):
+        return True
+    else:
+        return False
+
+print(check_permutation2("ABBBBBB", "BBBABBB"))
